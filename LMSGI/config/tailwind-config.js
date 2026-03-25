ac71fb251040cf7bd05219df21f91dd5c85237fa
Juan Garcia <jgagijon@proton.me>
2026-03-25 17:34:33 +0100

    Nuevo directorio /Components con header.html exportable. footer no exportable y reducido a menos líneas para su copiado en otras páginas

---
+++ LMSGI/config/tailwind-config.js
@@ -0,0 +1,35 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            /* Colores guardados para su uso a lo largo del proyecto */
+            colors: {
+                corporativo: '#ba007c',
+                musica: '#ba007c',
+                cine: '#009bdb',
+                teatro: '#009bdb',
+                exposiciones: '#ff7b00',
+                otros: '#555555',
+            },
+            fontFamily: {
+                asenine: ['Asenine', 'sans-serif'],
+                archistry: ['"Ar Chistry"', 'sans-serif'],
+            },
+            keyframes: {
+                /* Efecto visual para el símbolo & del logotipo.
+                El caracter alterna su opacidad entre los 4 colores corporativos propuestos
+                Cada 8 segundos, el símbolo & hace una rotación de color completa*/
+                ampColor: {
+                    '0%':   { color: '#009bdb' },
+                    '25%':  { color: '#ba007c' },
+                    '50%':  { color: '#555555' },
+                    '75%':  { color: '#ff7b00' },
+                    '100%': { color: '#009bdb' },
+                }
+            },
+            /*Definición de la animación para su uso en el proyecto*/
+            animation: {
+                'animacionLogo': 'ampColor 8s ease-in-out infinite',
+            }
+        }
+    }
+}
