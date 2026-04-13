ad5b2a87ae977f987880cd6167af7cc593910990
Juan García PC <jgagijon@proton.me>
2026-04-13 18:50:09 +0200

    Corregidos los scripts en evento con ID 5. La fecha del evento era errónea.

--- BADAT/Queries/InsercionEventos.sql
+++ BADAT/Queries/InsercionEventos.sql
@@ -6,5 +6,5 @@ VALUES
 ('Festival de Cortos', 'Antigua escuela de Comercio', 'Finalistas internacionlaes del concurso de cortos', '2026-01-12'),
 ('Charla LGTBIQ+', 'Centro Cultural', 'Aprende sobre inclusión, derechos y experiencias de la comunidad LGBTIQ+', '2026-01-24'),
 ('Arte contra la Guerra', 'Nicanor Piñole', 'La segunda guerra mundial vista desde el pincel de los artistas contemporáneos', '2026-06-15'),
-('Luces de Bohemia', 'Teatro Jovellanos', 'Representación del esperpento de Valle-Inclán', '2026-06-25'),
+('Luces de Bohemia', 'Teatro Jovellanos', 'Representación del esperpento de Valle-Inclán', '2026-06-15'),
 ('Requiem de Verdi', 'Auditorio Universidad Laboral', 'Interpretación a manos de la OSPA del requiem de Verdi', '2026-06-25'); 
